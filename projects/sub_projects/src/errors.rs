55582b4537d685799dd7f28df903230f44bf1003
Alexander Poole <alex.o.poole@gmail.com>
2024-01-13 12:11:16 +0100

    Add missing documentation

--- projects/sub_projects/src/errors.rs
+++ projects/sub_projects/src/errors.rs
@@ -1,6 +1,9 @@
 #[derive(Debug, Copy, Clone)]
+/// Error type for the sub_projects crate
 pub enum Error {
+    /// Error when the project is not found
     UnknownError
 }
 
+/// Result type for the sub_projects crate
 pub type Result<T> = std::result::Result<T, Error>;
